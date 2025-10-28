c449d8d383756a5d8a2c80e8dff3335c39e714a8
JoelsonJose <joelsonjose222@gmail.com>
2025-10-27 23:51:43 -0300

    implementação da tela de login

--- vamosjuntos/src/App.js
+++ vamosjuntos/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import PrimeiraPagina from './pages/paginasplash/primeirapagina'; 
 import PaginaInicial from './pages/PaginaInicial/PaginaInicial';
 import PaginaRegistro from './pages/PaginaRegistro/PaginaRegistro';
+import PaginaLogin from './pages/PaginaLogin/PaginaLogin';
 
 function App() {
   return (
@@ -9,8 +10,10 @@ function App() {
       <Route path= "/" element = {<PrimeiraPagina />} />
       <Route path= "/inicio" element= {<PaginaInicial />} />
       <Route path= "/cadastro" element= {<PaginaRegistro />} />
+      <Route path= "/login" element= {<PaginaLogin />} />
     </Routes>
   );
 }
 
 export default App;
+
