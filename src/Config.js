db0efaaf322157737da440f6fe5daa5cb9b8aadb
JoelsonJose <joelsonjose222@gmail.com>
2025-11-22 17:53:00 -0300

    configuração da API

--- src/Config.js
+++ src/Config.js
@@ -1,9 +1,12 @@
+// Lógica para descobrir onde o site está rodando
+const hostname = window.location.hostname;
 
+// Se estiver rodando no seu computador (localhost ou 127.0.0.1)
+const isLocal = hostname === "localhost" || hostname === "127.0.0.1";
 
-const isLocal = window.location.hostname === "localhost";
+const GITHUB_USER = "JoelsonJose"; 
+const GITHUB_REPO = "VamosJuntos";  
 
 export const API_URL = isLocal 
   ? 'http://localhost:3001' 
-  : 'https://my-json-server.typicode.com/JoelsonJose/VamosJuntos'; 
-
-fetch(`${API_URL}/rotas`) 
+  : `https://my-json-server.typicode.com/${GITHUB_USER}/${GITHUB_REPO}`; 
